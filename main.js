afa72a59f0a83aa11d35594aada2c1a6e5c64522
a-marquez <albert.marquez.2012@gmail.com>
2022-12-13 10:45:18 -0500

    Implement ui

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ const choices = {
 }
 
 function getComputerChoice() {
-  return choices[Math.ceil(Math.random() * 3)]
+  const choice = choices[Math.ceil(Math.random() * 3)]
+  document.getElementById('computer-choice').innerText = choice
+  document.getElementById('timestamp').innerText = new Date().toLocaleTimeString()
+  return choice
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -24,13 +27,42 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt()
-    let computerSelection = getComputerChoice()
-    let result = playRound(playerSelection, computerSelection)
-    console.log(`${playerSelection} vs ${computerSelection}: ${result}`)
-  }
+function reset() {
+  document.getElementById('outcome').innerText = ''
+  document.getElementById('timestamp').innerText = ''
+  document.getElementById('p1-score').innerText = 0
+  document.getElementById('cpu-score').innerText = 0
 }
 
-game()
+function playerThrow(choice) {
+  const outcome = playRound(choice, getComputerChoice())
+  const color = outcome == 'Draw'
+    ? 'yellow'
+    : outcome == 'You win'
+    ? 'green'
+    : 'red'
+
+  document.getElementById('outcome').innerText = outcome
+  document.getElementById('outcome').style.color = color
+
+  const p1ScoreEl = document.getElementById('p1-score')
+  const cpuScoreEl = document.getElementById('cpu-score')
+  let p1Score, cpuScore
+
+  if (outcome == 'You win') {
+    p1Score = parseInt(p1ScoreEl.innerText)
+    p1Score++
+    p1ScoreEl.innerText = p1Score
+  }
+  if (outcome == 'You lose') {
+    cpuScore = parseInt(cpuScoreEl.innerText)
+    cpuScore++
+    cpuScoreEl.innerText = cpuScore
+  }
+
+  if ([p1Score, cpuScore].includes(5)) {
+    alert(`${p1Score == 5 ? 'You' : 'The computer'} reached 5 wins!
+Press okay to reset.`)
+    reset()
+  }
+}
